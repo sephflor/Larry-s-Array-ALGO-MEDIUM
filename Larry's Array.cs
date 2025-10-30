c7d6870eb96639c7e53a912da0faa165fdf1dae9
Sephanne Flor <sephanneflor9@gmail.com>
2025-10-30 18:55:27 +0800

    Create Larry's Array.cs

---
+++ Larry's Array.cs
@@ -0,0 +1,68 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'larrysArray' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY A as parameter.
+     */
+
+    public static string larrysArray(List<int> A)
+    {
+        int n = A.Count;
+        int inversions = 0;
+
+        // Count inversions
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (A[i] > A[j])
+                    inversions++;
+            }
+        }
+
+        // If inversions count is even, array is sortable
+        return (inversions % 2 == 0) ? "YES" : "NO";
+    }
+
+    }
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> A = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(ATemp => Convert.ToInt32(ATemp)).ToList();
+
+            string result = Result.larrysArray(A);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
